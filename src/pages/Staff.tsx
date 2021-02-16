b64767ac34d46ccf48126d68eb06bc5314e98d2a
rjwu95 <yugun000@gmail.com>
2021-02-16 12:00:20 +0900

    Staff 목록 페이지 생성

--- src/pages/Staff.tsx
+++ src/pages/Staff.tsx
@@ -1,5 +1,58 @@
 import React from "react";
+import { Button, Table } from "antd";
+import { SearchBar, RegisterButton } from "../components";
+import { TableBlock } from "../utils/table";
 
+const { Column } = Table;
+
+const DELETE_KEY = "delete";
+
+const columns = [
+  {
+    title: "이름",
+    dataIndex: "name",
+  },
+  {
+    title: "아이디",
+    dataIndex: "loginId",
+  },
+  {
+    title: "관리",
+    dataIndex: DELETE_KEY,
+  },
+];
+
+const dataSource = [
+  {
+    key: 1,
+    name: "유건",
+    loginId: "yugun",
+  },
+];
 export const Staff = () => {
-  return <div>Staff</div>;
+  const onSearch = () => {};
+  const onDelete = (a: any) => {
+    console.log(a);
+  };
+
+  return (
+    <>
+      <SearchBar onFinish={onSearch} />
+      <TableBlock dataSource={dataSource}>
+        {columns.map((el) => (
+          <Column
+            title={el.title}
+            dataIndex={el.dataIndex}
+            key={el.dataIndex}
+            {...(el.dataIndex === DELETE_KEY && {
+              render: (col, data: typeof dataSource[0]) => (
+                <Button onClick={() => onDelete(data.loginId)}>삭제</Button>
+              ),
+            })}
+          />
+        ))}
+      </TableBlock>
+      <RegisterButton label="직원등록" link="/staff/register" />
+    </>
+  );
 };
