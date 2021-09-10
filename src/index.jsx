c897cba67cfa8a21801d22488969f14c950fe3d9
rjwu95 <yugun000@gmail.com>
2021-09-10 15:54:56 +0900

    change server host

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,9 @@ import { store, logout } from "./shared/store";
 import { Provider } from "react-redux";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/api";
+axios.defaults.baseURL =
+  // "http://localhost:3001/api";
+  "http://ec2-3-36-60-243.ap-northeast-2.compute.amazonaws.com:8080/api";
 axios.interceptors.request.use((req) => {
   req.headers.token = sessionStorage.getItem("token");
   return req;
